02b5a374a1e06dd68cb6edad307c9381f6c5f880
Rayd Trujillo <rayd1893@gmail.com>
2021-05-06 22:10:45 -0500

    Primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent  {
+
+  heroes:string[] = ['Spiderman','Ironman','Hulk','Thor'];
+  heroeBorrado:string = '';
+
+  borrarHeroe():void{
+    console.log('borrando...');
+
+    this.heroeBorrado = this.heroes.pop() || '';
+    
+  }
+
+}
